ef6d4845f10bce9543b4fee9fb4c22ae97fca7a2
oyugirachel <oyugirachel20@gmail.com>
2020-11-13 19:06:31 +0300

    initial commit

---
+++ forloop.py
@@ -0,0 +1,11 @@
+for i in range(100):
+    if i%3==0 and i%5 ==0:
+        print("Crackle Pop")
+    elif i%3==0:
+        print("Crackle")
+    elif i%5==0:
+        print("pop")
+    else:
+        print(i)
+
+    
